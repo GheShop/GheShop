693a866dd716cded0eaf195d5f70d22e7839197c
LeBaMinh <lemp@systena.co.jp>
2018-06-28 17:08:14 +0700

    add folder admin

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -5,9 +5,10 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
+    <link rel="icon" type="image/png" href="{{URL::asset('images/lockTitle.png')}}" />
+    <title>Login Admin</title>
     <link rel="stylesheet" href="{{URL::asset('css/bootstrap.css')}}" type="text/css">
-    <link rel="stylesheet" href="{{URL::asset('css/style.css')}}" type="text/css">
+    <link rel="stylesheet" href="{{URL::asset('css/backend/login.css')}}" type="text/css">
 </head>
 <body>
 <div class="row" id="loginTop">
